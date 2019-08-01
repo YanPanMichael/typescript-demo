c4e3c0632e80fd0347886bcf6e5491d9b8414767
YanPanMichael <yanpancsu@icloud.com>
2019-08-01 23:55:27 +0800

    enum polyfill

---
+++ src/demo/enum.js
@@ -0,0 +1,8 @@
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 2] = "Red";
+    Color[Color["Green"] = 3] = "Green";
+    Color[Color["Blue"] = 4] = "Blue";
+})(Color || (Color = {}));
+var colorName = Color[2];
+var color = Color.Blue;
