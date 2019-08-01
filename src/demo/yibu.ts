6ba122774f500cee31d151c31aefc190fa1e96d3
YanPanMichael <yanpancsu@icloud.com>
2019-08-02 00:59:53 +0800

    settimeout in for loop

--- src/demo/yibu.ts
+++ src/demo/yibu.ts
@@ -1,5 +1,19 @@
+for (var i = 0; i < 10; i++)  {
+    setTimeout(function() {
+      console.log('var, ', i)
+    }, 100 * i)
+}
+
+for (var i = 0; i < 10; i++)  {
+  (function(j){
+    setTimeout(function() {
+      console.log('var, gai, ', j)
+    }, 100 * j)
+  })(i)
+}
+
 for (let i = 0; i < 10; i++)  {
-  setTimeout(function() {
-    console.log(i)
-  }, 100 * i)
+    setTimeout(function() {
+      console.log('let, ', i)
+    }, 100 * i)
 }
