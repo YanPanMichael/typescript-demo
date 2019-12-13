007489e65d0b224018a5babfccb24f50fc707b3c
YanPanMichael <yanpancsu@icloud.com>
2019-12-13 23:59:25 +0800

    suspense

---
+++ src/demo/suspense.ts
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SuspenseDemo extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SuspenseDemo
