84ccfd36ebed6b95c87015ba4e7eaaf315ddf844
YanPanMichael <yanpancsu@icloud.com>
2019-12-18 00:02:14 +0800

    save

--- src/demo/function.ts
+++ src/demo/function.ts
@@ -1,3 +1,13 @@
 let myFunc2: (x:number, y:number) => number = function(x:number, y:number):number {
   return x + y
 }
+
+function buildName(fn:string, ln?:string):string {
+  return fn + ' ' + ln
+}
+
+function buildName2(fn:string, ...restName:string[]):string {
+  return fn + ' ' + restName
+}
+
+buildName('Bob')
