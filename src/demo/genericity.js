31232c7caa3962cd0091bcb3c7b6e9f015cccdf5
YanPanMichael <yanpancsu@icloud.com>
2019-12-21 22:39:57 +0800

    genericInterface

---
+++ src/demo/genericity.js
@@ -0,0 +1,7 @@
+function identity(arg) {
+    return arg;
+}
+var t1 = identity('input string');
+var t2 = identity('input string');
+// 赋值
+var identity2 = identity;
--- src/demo/genericity.ts
+++ src/demo/genericity.ts
@@ -4,3 +4,20 @@ function identity<T>(arg: T):T {
 
 let t1 = identity<string>('input string');
 let t2 = identity('input string');
+
+// 赋值
+const identity2: <T>(arg: T) => T = identity;
+
+// 泛型参数抽取成为接口
+const identity3: {<T>(arg: T[]): T[]} = identity;
+
+interface genericIdentify {
+  <T>(arg:T): T
+}
+const identity4: genericIdentify = identity;
+
+// 提出T 可以使得类型作为一个变量传递
+interface genericIdentify2<T> {
+  (arg:T): T
+}
+const identity5: genericIdentify2<number> = identity;
