d8be0b56c2da554996755b090a28e72959ba88fd
YanPanMichael <yanpancsu@icloud.com>
2019-12-22 01:22:13 +0800

    extends generic

--- src/demo/genericity.js
+++ src/demo/genericity.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function identity(arg) {
     return arg;
 }
@@ -9,3 +22,48 @@ var identity2 = identity;
 var identity3 = identity;
 var identity4 = identity;
 var identity5 = identity;
+function genericIdentify3(arg) {
+    console.log(arg.length);
+    return arg;
+}
+function getProperty(obj, key) {
+    return obj[key];
+}
+var o = { a: 1, b: 2, c: 3, d: 4 };
+getProperty(o, 'a');
+// getProperty(o, 'e');
+//工厂类
+var BeeKeeper = /** @class */ (function () {
+    function BeeKeeper() {
+    }
+    return BeeKeeper;
+}());
+var LionKeeper = /** @class */ (function () {
+    function LionKeeper() {
+    }
+    return LionKeeper;
+}());
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    return Animal;
+}());
+var Bee = /** @class */ (function (_super) {
+    __extends(Bee, _super);
+    function Bee() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Bee;
+}(Animal));
+var Lion = /** @class */ (function (_super) {
+    __extends(Lion, _super);
+    function Lion() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Lion;
+}(Animal));
+function createInstance(cons) {
+    return new cons();
+}
+createInstance(Bee).keeper.nameTag;
+createInstance(Lion).keeper.nameTag;
--- src/demo/genericity.ts
+++ src/demo/genericity.ts
@@ -21,3 +21,48 @@ interface genericIdentify2<T> {
   (arg:T): T
 }
 const identity5: genericIdentify2<number> = identity;
+
+interface TextendInferface {
+  length: number
+}
+
+function genericIdentify3<T extends TextendInferface>(arg: T): T { //通过继承接口 接口中有length属性 实现.length不报错
+  console.log(arg.length);
+  return arg;
+}
+
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+let o = {a: 1, b: 2, c: 3, d: 4};
+getProperty(o, 'a');
+// getProperty(o, 'e');
+
+//工厂类
+class BeeKeeper {
+  nameTag!: string
+}
+
+class LionKeeper {
+  nameTag!: string
+}
+
+class Animal {
+  numLengs!: number
+}
+
+class Bee extends Animal {
+  keeper!: BeeKeeper
+}
+
+class Lion extends Animal {
+  keeper!: LionKeeper
+}
+
+function createInstance<T extends Animal>(cons: new() => T): T { // 参数为构造函数类型 new() => T
+  return new cons()
+}
+
+createInstance(Bee).keeper.nameTag
+createInstance(Lion).keeper.nameTag
